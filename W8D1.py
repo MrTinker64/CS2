9b6bc5f76ecb40f2d6d0dd9bb7c5b756d83a76f0
MrTinker64 <avi.micah@gmail.com>
2025-02-04 09:50:48 -0800

    add w8d1

---
+++ W8D1.py
@@ -0,0 +1,63 @@
+'''
+   Problem 2
+'''
+#Your code here
+
+
+'''
+   Problem 3
+'''
+#Your code here
+
+'''
+   Problem 4
+'''
+def mySum(myList):
+    return
+
+# Code for Problems 5
+
+def subtractor(x):
+    def inner(y):
+        return x-y
+    return inner
+
+
+def myFunc(x):
+    def rev(y):
+        if len(y)==0:
+            return x
+        else:
+            return y[-1]+rev(y[:-1])
+    return rev
+
+'''
+   Problem 6
+'''
+def applyFuncs(myList,n):
+    #Your code here
+    return
+
+'''
+   Problem 7
+'''
+def summation(n,f):
+    #Your code here
+    return
+
+'''
+   Problem 8
+'''
+def couple(list1,list2):
+    #Your code here
+    return
+
+'''
+   Problem 9
+'''
+def num_eights(number):
+    #Your code here
+    return
+
+
+
