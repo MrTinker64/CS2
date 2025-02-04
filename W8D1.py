8c276001a80f04c312527fa76309e50f89c9b79c
MrTinker64 <avi.micah@gmail.com>
2025-02-04 09:55:28 -0800

    add problem 1

--- W8D1.py
+++ W8D1.py
@@ -1,4 +1,12 @@
 '''
+   Problem 1
+'''
+print([x * 2 for x in range(4) if x % 2 == 1]) # [2, 6]
+print([letter for letter in 'Urban' if letter < 'm']) # ['U', 'b', 'a']
+print("".join([letter for letter in 'Urban' if letter < 'm'])) # Uba
+print("".join([word[0] for word in "Go Blues!".split() if not(len(word) == 2)])) # B
+
+'''
    Problem 2
 '''
 #Your code here
