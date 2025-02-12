dd94bb8d4742760c548815e3f310b108dd79c4c5
MrTinker64 <avi.micah@gmail.com>
2025-02-12 09:05:31 -0800

    2

--- W9D2.py
+++ W9D2.py
@@ -3,4 +3,7 @@ adj = [[0,1,0,0,1],[1,0,1,1,1],[0,1,0,1,0],[0,1,1,0,1],[1,1,0,1,0]]
 def vertexDegree(vertex, graph):
     return sum(graph[vertex])
 
-print(vertexDegree(1,adj))
+def numberOddDegreeVertices(graph):
+    return len([v for v in adj if vertexDegree(graph.index(v), graph) % 2 != 0])
+
+print(numberOddDegreeVertices(adj))
