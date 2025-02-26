9b40d7c6fa8263230d66bff52a2d8d176a6732ae
MrTinker64 <avi.micah@gmail.com>
2025-02-26 08:44:47 -0800

    make friends graph

---
+++ finalOfCS2/Inviting Friends to a Party.py
@@ -0,0 +1,20 @@
+import random
+
+
+def friends(n):
+    graph = []
+    for i in range(n):
+        if i == 0:
+            graph = [[0] + randomWeights(n - 1)]
+        else:
+            graph += [randomWeights(i) + [0] + randomWeights(n - i - 1)]
+    return graph
+
+
+def randomWeights(i) -> list:
+    return [random.randint(0, 1) for _ in range(i)]
+
+
+graph = friends(5)
+for line in graph:
+    print(f"{line},")
