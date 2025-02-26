0e1e3c1d3fd58d72dad7b969b05757873ad432ef
MrTinker64 <avi.micah@gmail.com>
2025-02-26 08:48:21 -0800

    isClique works

--- finalOfCS2/Inviting Friends to a Party.py
+++ finalOfCS2/Inviting Friends to a Party.py
@@ -15,6 +15,29 @@ def randomWeights(i) -> list:
     return [random.randint(0, 1) for _ in range(i)]
 
 
-graph = friends(5)
+def isClique(cq, graph):
+    for vertex in cq:
+        neighbors = neighborhood(vertex, graph)
+        for v in cq:
+            if neighbors.count(v) < 1 and v != vertex:
+                return False
+    return True
+
+
+def neighborhood(vertex, graph):
+    edges = graph[vertex]
+    connections = []
+    for i in range(len(edges)):
+        edge = edges[i]
+        if edge != 0:
+            connections.append(i)
+    return connections
+
+
+graph = friends(3)
 for line in graph:
     print(f"{line},")
+
+cq = [0, 1, 2]
+
+print(f"Is {cq} clique: {isClique(cq, graph)}")
