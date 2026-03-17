eec49aaaa5471b3c261de63694f29e83b9825b55
MrTinker64 <avi.micah@gmail.com>
2026-03-17 14:10:28 -0700

    problem 1

---
+++ CS3/dictionaries.py
@@ -0,0 +1,8 @@
+class_dict = {'Math':'1A', 'English': 'Shakespeare', 'CS':'3'}
+
+# class_dict['Math'] = '1B'
+# print(class_dict['Math'])
+
+# print('Philosophy' in class_dict)
+# print('10' in class_dict.values())
+# print(class_dict.keys())
