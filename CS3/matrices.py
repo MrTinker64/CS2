5d49faea8af230ed9eb137ca0d11453768ba355b
MrTinker64 <avi.micah@gmail.com>
2026-03-17 13:01:53 -0700

    see what another one looks like

--- CS3/matrices.py
+++ CS3/matrices.py
@@ -1,6 +1,7 @@
 A = [[1, 2], [3, 4]]
 B = [[5, 6], [7, 8]]
 L = [[9, -18, 27], [36, 55, 54]]
+D = [[1, 2]]
 
 def add(X, Y):
     return [[X[i][j] + Y[i][j] for j in range(len(X))] for i in range(len(X))]
