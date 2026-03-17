6ad40d95598690e67d2ec366e8dd6d96bbd7fe4f
MrTinker64 <avi.micah@gmail.com>
2026-03-17 14:15:03 -0700

    part 5a

--- CS3/dictionaries.py
+++ CS3/dictionaries.py
@@ -6,3 +6,11 @@ class_dict = {'Math':'1A', 'English': 'Shakespeare', 'CS':'3'}
 # print('Philosophy' in class_dict)
 # print('10' in class_dict.values())
 # print(class_dict.keys())
+
+fav_numbers = {'Henri': 68, 'Richard': 19, 'Parisa': 42, 'Jessica': 4}
+nums = [68, 42]
+
+for person in fav_numbers:
+    fav_numbers[person] += len(list(person))
+    
+print(fav_numbers)
