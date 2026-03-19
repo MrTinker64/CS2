fe2f4bdd5ad6c7f340810ed4c11d1b5c41dd3819
MrTinker64 <avi.micah@gmail.com>
2026-03-19 13:22:30 -0700

    fix formatting

--- CS3/dictionaries2.py
+++ CS3/dictionaries2.py
@@ -1,3 +1,6 @@
+"""
+Problem 1
+"""
 def frequency(myString):
     new_dict = {}
     for letter in myString:
@@ -8,6 +11,9 @@ def frequency(myString):
 
 print(frequency("abracadabra"))
 
+"""
+Problem 2
+"""
 def most_freq_seq(myString, length):
     seqs = [myString[i:i + length] for i in range(len(myString) - length + 1)]
     counts = frequency(seqs)
@@ -21,6 +27,9 @@ def most_freq_seq(myString, length):
 
 #print(most_freq_seq("abracadabra", 4))
 
+"""
+Problem 3
+"""
 def substitute_chars(myString, myDict):
     result = ""
     for char in myString:
@@ -30,6 +39,9 @@ def substitute_chars(myString, myDict):
 #replacements = {'S':'Z', 'E':'U', 'T':'P', 'A':'M'}
 #print(substitute_chars("SECRET MESSAGE",replacements))
 
+"""
+Problem 4
+"""
 def invert_dict(myDict):
     new_dict = {}
     for key in myDict:
@@ -42,9 +54,21 @@ original = {'a': 5, 'b': 2, 'c': 1}
 
 print(invert_dict(frequency("abracadabra")))
 
-'''
+"""
+Problem 5
+Enter your answer inside this comment.
+
 {'a': 5, 'b': 2, 'r': 2, 'c': 1, 'd': 1}
 {5: 'a', 2: 'r', 1: 'd'}
-5. it overwrites the previous value. Because both "b" and "r" correspond to 2 the code first sets 2:"b" and then overwrites that with 2:"r"
-6. M was never changed by the substitution, therefore the inverted substitution accidentally changes it. the only way to get around this is to make sure the keys contain all values and the values contain all keys
-'''
+it overwrites the previous value. Because both "b" and "r" correspond to 2 the code first sets 2:"b" and then overwrites that with 2:"r"
+
+"""
+
+
+"""
+Problem 6
+Enter your answer inside this comment.
+
+M was never changed by the substitution, therefore the inverted substitution accidentally changes it. the only way to get around this is to make sure the keys contain all values and the values contain all keys
+
+"""
