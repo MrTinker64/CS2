d991a95c2946a6368a5a344c8f92f6e3c6062eb9
MrTinker64 <avi.micah@gmail.com>
2026-03-23 08:59:04 -0700

    dosomething

--- CS3/my_turtle.py
+++ CS3/my_turtle.py
@@ -8,9 +8,17 @@ s.setup(1000,1000)
 s.title('CS 3')
 s.bgcolor('skyblue')
 
-for i in range(360):
-    t.forward(1)
-    t.left(1)
+# for i in range(360):
+#     t.forward(1)
+#     t.left(1)
+
+# def doSomething(x,y):
+#    t.goto(x,y)
+#    t.shape('circle')
+#    t.shapesize(2.5,2.5,2.5)
+#    t.stamp()
+
+# s.onscreenclick(doSomething)
 
 turtle.update()
 turtle.mainloop()
