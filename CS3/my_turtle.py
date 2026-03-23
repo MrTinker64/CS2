4fb08d17372eacb203972ff2a11d5d7a56c8d4bb
MrTinker64 <avi.micah@gmail.com>
2026-03-23 08:57:29 -0700

    screen

--- CS3/my_turtle.py
+++ CS3/my_turtle.py
@@ -3,6 +3,11 @@ import turtle
 turtle.tracer(0)
 t = turtle.Turtle()
 
+s = turtle.Screen()
+s.setup(1000,1000)
+s.title('CS 3')
+s.bgcolor('skyblue')
+
 for i in range(360):
     t.forward(1)
     t.left(1)
