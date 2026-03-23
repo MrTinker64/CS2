cfd090a2d587b601f6c3377e6c2385ea7bcaf34c
MrTinker64 <avi.micah@gmail.com>
2026-03-23 09:12:24 -0700

    add square on click

--- CS3/my_turtle.py
+++ CS3/my_turtle.py
@@ -1,12 +1,24 @@
 import turtle
 
-turtle.tracer(0)
 t = turtle.Turtle()
 
 s = turtle.Screen()
 s.setup(1000,1000)
 s.title('CS 3')
 s.bgcolor('skyblue')
+
+def squareOnClick(x, y):
+    turtle.tracer(0)
+    length = 100
+    t.penup()
+    t.goto(x-length/2, y-length/2)
+    t.pendown()
+    for i in range(4):
+        t.forward(length)
+        t.left(90)
+    turtle.update()
+
+s.onscreenclick(squareOnClick)
 
 # for i in range(360):
 #     t.forward(1)
@@ -20,28 +32,27 @@ s.bgcolor('skyblue')
 
 # s.onscreenclick(doSomething)
 
-def square1(length):
-    t.color("blue")
-    t.penup()
-    t.right(135)
-    t.forward(length * (2 ** 0.5) / 2)
-    t.left(135)
-    t.pendown()
-    for i in range(4):
-        t.forward(length)
-        t.left(90)
+# def square1(length):
+#     t.color("blue")
+#     t.penup()
+#     t.right(135)
+#     t.forward(length * (2 ** 0.5) / 2)
+#     t.left(135)
+#     t.pendown()
+#     for i in range(4):
+#         t.forward(length)
+#         t.left(90)
         
-def square2(length):
-    t.color("red")
-    t.penup()
-    t.goto(-length/2, -length/2)
-    t.pendown()
-    for i in range(4):
-        t.forward(length)
-        t.left(90)
+# def square2(length):
+#     t.color("red")
+#     t.penup()
+#     t.goto(-length/2, -length/2)
+#     t.pendown()
+#     for i in range(4):
+#         t.forward(length)
+#         t.left(90)
         
-square1(100)
-square2(110)
+# square1(100)
+# square2(110)
 
-turtle.update()
 turtle.mainloop()
