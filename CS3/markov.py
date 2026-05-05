308b63b16bafae6f42f2ed129d8d696613481eb1
MrTinker64 <avi.micah@gmail.com>
2026-05-05 14:23:45 -0700

    2 and 3!

--- CS3/markov.py
+++ CS3/markov.py
@@ -1,5 +1,16 @@
+my_model = {}
+
 def get_k_gram(text, position, k):
-    text+=text
+    text+=text+text[0]
     return text[position:position+k]
 
-print(get_k_gram('parisa', 5, 4))
+def add_char_to_model(model, text, position, k):
+    k_gram = get_k_gram(text, position, k)
+    if k_gram not in model:
+        model[k_gram] = {text[position+k]: 1}
+    else:
+        model[k_gram][text[position+k]] += 1
+
+for i in range(3):
+	 	add_char_to_model(my_model, 'fald fall', i, 3)
+print(my_model)
