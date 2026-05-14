1a571e46db554cf211abaf60cd74ee2969c78fb9
MrTinker64 <avi.micah@gmail.com>
2026-05-14 13:27:12 -0700

    add some stuff

--- CS3/Adventure Game/data.py
+++ CS3/Adventure Game/data.py
@@ -4,28 +4,19 @@ from classes import *
 
 """ Your characters, e.g.:
 parisa = Character('Parisa', 'Life is Math!')
-christina = Character('Christina', 'Sign in!')
 """
+christina = Character('Christina', 'Sign in!')
 
 # Things:
-""" Your things, e.g.: 
 coffee = Thing('Coffee', 'The caffienated nectar of the gods.')
-"""
 
 # Keys:
-try:
-    skeleton_key = Key('Skeleton Key', 'A key that unlocks many doors')
-except NameError as e:
-    skeleton_key = Thing('Not a Skeleton Key', 'You must first implement the Key class')
+skeleton_key = Key('Skeleton Key', 'A key that unlocks many doors')
 
+# (self, name, description, characters, things)
 
 # Places:
-""" Your places, e.g:
-front_desk = Place('Front Desk','I don\'t know why you say goodbye I say hello.', [christina], ['trivia question board'])
-"""
-
-
-
+front_desk = Place('Front Desk','I don\'t know why you say goodbye I say hello.', [christina], [coffee])
 
 # Exits:
 """Your exits, e.g.:
@@ -37,10 +28,5 @@ front_desk.add_exits([admissions, conference_room, haight_street])
 front_desk.locked = False
 """
 
-
 # Player:
-"""Initialize your player, e.g.:
 me = Player('BYI',front_desk)
-"""
-
-
