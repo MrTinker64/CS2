8c03e4679a07172b44ee473b8f33a4f49eb620cd
MrTinker64 <avi.micah@gmail.com>
2026-05-19 16:24:07 -0700

    add more descriptions and todos

--- CS3/Adventure Game/classes.py
+++ CS3/Adventure Game/classes.py
@@ -14,7 +14,7 @@ class Player:
         # TODO test this
         print('You are currently in the ' + self.place.name, end=". ")
         if self.place.name == 'Storage Closet':
-            if not 'Flashlight' in self.backpack:
+            if not any(item.name == 'Flashlight' for item in self.backpack):
                 print("The room is too dark to see in and the only light has broken.")
                 return
             else:
@@ -24,7 +24,7 @@ class Player:
         self.place.look()
         if self.place.name == 'Observatory' and self.night_time:
             print("Now that it is the early morning you are able to look through the telescope at the celestial objects listed on the wall.\nSoon you realize that all the objects you're looking at from double star clusters to binary stars or all manner of thin and cigar-shaped galaxies everything looks like either an 8 or a 1.\nBy the time you're done watching it has become day again.")
-        if self.place.name == 'Lake' and 'Remote Controller' in self.backpack:
+        if self.place.name == 'Lake' and any(item.name == 'Remote Controller' for item in self.backpack):
             print("You use the remote controller to drive the toy boat over to you. Sitting inside are some blueprints.\nThe blue prints seem to be for the very estate that your standing on. Though the left half has been damaged by water you can make out the lake and the building to the right.\nFrom this perspective they look like a 0 and a 9.")
 
     def go_to(self, location):
--- CS3/Adventure Game/data.py
+++ CS3/Adventure Game/data.py
@@ -2,25 +2,25 @@ from classes import *
 
 # Things:
 lockbox = Thing('Lockbox', 'A locked box with 4 dials all set to 0')
-# TODO remote controller
-# TODO toy boat with blue prints of this house that got water damaged so you can't make them out
+remote_controller = Thing('Remote Controller', '') # TODO describe these two
+flashlight = Thing('Flashlight', '')
 
 # Keys:
-skeleton_key = Key('Skeleton Key', 'A key that unlocks many doors')
+gear_key = Key('Gear Key', 'A gear-shaped item that looks like it might unlock something.')
 
 # Places:
 path = Place('Path','A straight gravel path from the dining room to the lake. Along the tree-lined path you make out two large circular buildings.', [])
-observatory = Place('Observatory','A round room with a large telescope in the middle. On the wall you see a chart with instructions for how to see various celestial bodies.', [])
-workshop = Place('Workshop','You see a remote controller lying on a shelf.', [])
+observatory = Place('Observatory','A round room with a large telescope in the middle. On the wall you see a chart with instructions for how to see various celestial bodies.', [flashlight])
+workshop = Place('Workshop','You see a remote controller lying on a shelf.', [remote_controller])
 
 dining_room = Place('Dining Room','A long room with a long table running down the middle and chairs running all along it. A locked box sits in the middle of the table.', [lockbox])
-kitchen = Place('Kitchen','The kitchen', [])
-pantry = Place('Pantry','A room shaped like a right triangle', [])
+kitchen = Place('Kitchen','The kitchen', []) # TODO come up with some fun food descriptions. Maybe desserts shaped like punctuation?
+pantry = Place('Pantry','A room shaped like a right triangle', [gear_key])
 guest_bedroom = Place('Guest Bedroom','A bedroom with a thank you note addressed to Mrs. G.\nThe guest remarked about the unique floorplan of this estate.', [])
 
-lake = Place('Lake','A path leads out about 10 feet to an oval shaped lake. You see something in the middle of the lake. When you squint closer it looks like a toyboat.', [])
+lake = Place('Lake','A path leads out about 10 feet to an oval shaped lake. You see something in the middle of the glittering lake. As you squint against the glare, it looks like a little toy boat.', [])
 arch_studio = Place('Architecture Studio','Collection of model buildings shaped like letters of the alphabet when viewed from above.', [])
-storage_closet = Place('Storage Closet','', [])
+storage_closet = Place('Storage Closet','', []) # TODO describe. Messy, dusty, architecture supplies, foam core, markers and velum, rulers, wood, old models. Maybe a even a number shaped building???
 zendo = Place('Zendo','', [])
 
 # Exits:
@@ -29,7 +29,7 @@ path.add_exits([observatory, dining_room, workshop])
 
 # one
 dining_room.add_exits([kitchen, lake, guest_bedroom])
-kitchen.add_exits([pantry])
+pantry.add_exits([kitchen])
 
 # zero
 lake.add_exits([arch_studio])
@@ -40,9 +40,7 @@ storage_closet.add_exits([arch_studio, zendo])
 
 
 # Locked places
-"""Your locked places, e.g.:
-front_desk.locked = False
-"""
+workshop.locked = True
 
 # Player:
 me = Player('Heir',dining_room)
