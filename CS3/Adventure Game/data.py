d903087f4e8fb4362195637ce5c2325f140095f5
MrTinker64 <avi.micah@gmail.com>
2026-05-19 16:40:58 -0700

    add some descriptions

--- CS3/Adventure Game/data.py
+++ CS3/Adventure Game/data.py
@@ -2,20 +2,21 @@ from classes import *
 
 # Things:
 lockbox = Thing('Lockbox', 'A locked box with 4 dials all set to 0')
-remote_controller = Thing('Remote Controller', '') # TODO describe these two
+remote_controller = Thing('Remote Controller', '') # TODO describe these 3
 flashlight = Thing('Flashlight', '')
+snacks = Thing('Snacks', '') # TODO come up with some fun food descriptions. Maybe desserts shaped like punctuation?
 
 # Keys:
 gear_key = Key('Gear Key', 'A gear-shaped item that looks like it might unlock something.')
 
 # Places:
-path = Place('Path','A straight gravel path from the dining room to the lake. Along the tree-lined path you make out two large circular buildings.', [])
-observatory = Place('Observatory','A round room with a large telescope in the middle. On the wall you see a chart with instructions for how to see various celestial bodies.', [flashlight])
-workshop = Place('Workshop','You see a remote controller lying on a shelf.', [remote_controller])
+path = Place('Path','A straight gravel path leading into the woods. Along the tree-lined path you make out two large circular buildings 30 feet away.', [])
+observatory = Place('Observatory','A round room with a large telescope in the middle 20 feet in diameter. On the wall you see a chart with instructions for how to see various celestial bodies.', [flashlight])
+workshop = Place('Workshop','Another circular room 20 feet in diameter. The walls are lined with shelves full of part way finished projects and different tools. A remote controller is lying in the middle of a work table near you.', [remote_controller])
 
 dining_room = Place('Dining Room','A long room with a long table running down the middle and chairs running all along it. A locked box sits in the middle of the table.', [lockbox])
-kitchen = Place('Kitchen','The kitchen', []) # TODO come up with some fun food descriptions. Maybe desserts shaped like punctuation?
-pantry = Place('Pantry','A room shaped like a right triangle', [gear_key])
+kitchen = Place('Kitchen','A clean, modern looking kitchen that is a 10x10 foot square with a door to your left. Lots of stainless-steel surfaces and spotless white tile floors. You check the cupboard and find some snacks.', [snacks])
+pantry = Place('Pantry','The pantry is a right triangle, sloping back towards the dining room. The shelves seem oddly empty, but a gear catches your eye.', [gear_key])
 guest_bedroom = Place('Guest Bedroom','A bedroom with a thank you note addressed to Mrs. G.\nThe guest remarked about the unique floorplan of this estate.', [])
 
 lake = Place('Lake','A path leads out about 10 feet to an oval shaped lake. You see something in the middle of the glittering lake. As you squint against the glare, it looks like a little toy boat.', [])
