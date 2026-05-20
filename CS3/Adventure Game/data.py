2fe7686be490b6926de84085583ecca2dc6fac48
MrTinker64 <avi.micah@gmail.com>
2026-05-19 17:33:26 -0700

    update descriptions

--- CS3/Adventure Game/data.py
+++ CS3/Adventure Game/data.py
@@ -10,16 +10,16 @@ snacks = Thing('Snacks', '') # TODO come up with some fun food descriptions. May
 gear_key = Key('Gear Key', 'A gear-shaped item that looks like it might unlock something.')
 
 # Places:
-path = Place('Path','A straight gravel path leading into the woods. Along the tree-lined path you make out two large circular buildings 30 feet away.', [])
+path = Place('Path','A straight gravel path leading into the woods, it is about 10 ft wide. One either side of the tree-lined path you make out two large circular buildings 30 feet away.', [])
 observatory = Place('Observatory','A round room with a large telescope in the middle 20 feet in diameter. On the wall you see a chart with instructions for how to see various celestial bodies.', [flashlight])
 workshop = Place('Workshop','Another circular room 20 feet in diameter. The walls are lined with shelves full of part way finished projects and different tools. A remote controller is lying in the middle of a work table near you.', [remote_controller])
 
-dining_room = Place('Dining Room','A long room with a long table running down the middle and chairs running all along it. A locked box sits in the middle of the table.', [lockbox])
+dining_room = Place('Dining Room','An ornately carved long room. Standing in the center it stretches 15 ft in either direction and is 10 ft wide.\nThere is a long table running down the middle and chairs all around it. A locked box sits in the middle of the table.\nTo your right is the kitchen, the left is a bedroom, ahead is a path into the woods, and behind is a beautiful lake.', [lockbox])
 kitchen = Place('Kitchen','A clean, modern looking kitchen that is a 10x10 foot square with a door to your left. Lots of stainless-steel surfaces and spotless white tile floors. You check the cupboard and find some snacks.', [snacks])
-pantry = Place('Pantry','The pantry is a right triangle, sloping back towards the dining room. The shelves seem oddly empty, but a gear catches your eye.', [gear_key])
-guest_bedroom = Place('Guest Bedroom','A bedroom with a thank you note addressed to Mrs. G.\nThe guest remarked about the unique floorplan of this estate.', [])
+pantry = Place('Pantry','The pantry is an isosceles right triangle with one short edge flush with the kitchen and the other on your left.\nThe shelves are full with dried and canned goods like flour, sugar, pickles, pears, etc. Amongst all the food you see a little glimmer of metal.', [gear_key])
+guest_bedroom = Place('Guest Bedroom','A bedroom with a thank you note addressed to Mrs. G. Most of it is fluffy nice things, but you notice the guest emphasizing the odd and unique floorplan of this estate. The room goes 15ft in either direction and 10ft back.', [])
 
-lake = Place('Lake','A path leads out about 10 feet to an oval shaped lake. You see something in the middle of the glittering lake. As you squint against the glare, it looks like a little toy boat.', [])
+lake = Place('Lake','A path leads out about 10 feet to an oval shaped lake. Lake might be a generous term as it is only 30 feet across and 50 feet wide.\nYou see something in the middle of the glittering lake. As you squint against the glare, it looks like a little toy boat.', [])
 arch_studio = Place('Architecture Studio','Collection of model buildings shaped like letters of the alphabet when viewed from above.', [])
 storage_closet = Place('Storage Closet','', []) # TODO describe. Messy, dusty, architecture supplies, foam core, markers and velum, rulers, wood, old models. Maybe a even a number shaped building???
 zendo = Place('Zendo','', [])
