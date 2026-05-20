0665874f80b93ec50c4489d7978774f56c14bdb4
MrTinker64 <avi.micah@gmail.com>
2026-05-19 23:00:16 -0700

    leave more hints

--- CS3/Adventure Game/adventure.py
+++ CS3/Adventure Game/adventure.py
@@ -145,6 +145,8 @@ Good luck,
 Mrs. G
 Renowned architect for the following projects: the Rising Fire house, the Entire Slate building, and the CisAmerica Pyramid
 
+P.S. I will leave you with one hint. An architects best friend was a piece of graph paper and a pencil.
+
 """
 
 WIN_MESSAGE = """
