3ed37027381c5f3b9f858282967d13c5d3791ec4
MrTinker64 <avi.micah@gmail.com>
2026-05-20 21:35:08 -0700

    update descriptions to match layout

--- CS3/Adventure Game/classes.py
+++ CS3/Adventure Game/classes.py
@@ -11,7 +11,6 @@ class Player:
 
     def look(self):
         print('You are currently in the ' + self.place.name, end=". ")
-        # TODO match new layout
         if self.place.name == 'Closet':
             if not any(item.name == 'Flashlight' for item in self.backpack):
                 print("The room is too dark to see in and the only light has broken.")
@@ -27,12 +26,12 @@ class Player:
         if self.place.name == 'Observatory':
             print()
             if self.night_time:
-                print("Now that it is the early morning you are able to look through the telescope at the celestial objects listed on the wall.\nEventually you see an odd pattern emerging in the objects you're looking at.\nFirst you saw lots of double star clusters and binary stars which looked like an 8.\nFurther down the list there was a variety thin or cigar-shaped galaxies reminding you of a 1.\nBy the time you're done watching both the room, and your brain, have been enlightened.")
+                print("Now that it is the early morning you are able to look through the telescope at the celestial objects listed on the wall.\nEventually you see an odd pattern emerging in the objects you're looking at.\nFirst there was a variety thin or cigar-shaped galaxies reminding you of a 1.\nFurther down the list you saw lots of double star clusters and binary stars which all looked like an 8.")
             else:
                 print("Unfortunately you can't use the telescope right now because it is daytime.")
         if self.place.name == 'Lake' and any(item.name == 'RC' for item in self.backpack):
             print()
-            print("You use the remote controller to drive the toy boat over to you. Sitting inside are some blueprints.\nThe blue prints seem to be for the very estate that your standing on.\nThough the left half has been damaged by water you can make out the lake and architecture studio on the right half of the sheet.\nFrom the top-down view of the blueprint they look like a 0 and a 9.")
+            print("You use the remote controller to drive the toy boat over to you. Sitting inside are some blueprints.\nThe blue prints seem to be for the very estate that your standing on.\nThough the right half has been damaged by water you can make out the lake and architecture studio on the left half of the sheet.\nFrom the top-down view of the blueprint they look like a 9 and a 0.")
         print()
         self.place.look()
 
